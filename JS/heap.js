958d3c16ed51bf7abc71eb3be43bb5e34e3b9283
Amjad Ali (MJ) <amjad.hamdi.ali@gmail.com>
2018-10-14 16:44:21 -0500

    Defined another function heapSort

--- JS/heap.js
+++ JS/heap.js
@@ -6,4 +6,9 @@ const heapify = (arr, indx, size) => {
 
 }
 
-// Function takes 
+// Function heapifies array and swaps root with last leaf node
+// Then "Deletes" last leaf node. Process is repeated until no more nodes remain. 
+// Params: an array.
+const heapSort = (arr) => {
+	
+}
